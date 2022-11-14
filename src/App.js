0a9f8477ce1eaf51bdd71a68d00c3fe0545def97
Marvelous Ezechi E <marvelousezechi0@gmail.com>
2022-11-14 11:22:26 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import PlaceToStay from './Pages/PlaceToStay'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/place-to-stay" element={<PlaceToStay />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Card from '../components/Card';
+
+const Home = () => {
+
+    return (
+        <div className='main'>
+            <Navbar />
+            
+            <section className='section-1'>
+
+            </section>
+
+            <Card />
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
